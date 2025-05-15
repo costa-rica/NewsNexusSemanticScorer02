2e8635d26621b1b1db839b5a6444de3ed06aedcf
Nick <nrodrig1@gmail.com>
2025-05-15 12:35:26 +0200

    refactor: score on description, if no description us the approvedAriticle.textForPdfReport, if no textForPdfReport, use title, if not title do not score

--- modules/utilitiesScorer.js
+++ modules/utilitiesScorer.js
@@ -9,13 +9,22 @@ function cosineSimilarity(vecA, vecB) {
 }
 
 async function scoreArticleWithKeywords(article, keywords, embedder) {
-  if (!article.description) {
-    console.log(`article ${article.id} has no description`);
+  let descriptionUsedToScore = article.description;
+
+  if (!descriptionUsedToScore) {
+    console.log(
+      `article ${article.id} has no description, using title instead`
+    );
+    descriptionUsedToScore = article.title;
+  }
+
+  if (!descriptionUsedToScore) {
+    console.log(`article ${article.id} has no description, no title either`);
     return { keyword: null, keywordRating: null };
   }
 
   const articleVec = (
-    await embedder(article.description, {
+    await embedder(descriptionUsedToScore, {
       pooling: "mean",
       normalize: true,
     })
